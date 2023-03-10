c3ade208989c3a0b2c1447f0ea0ba98867464753
DylanovE <90382484+DylanovE@users.noreply.github.com>
2023-03-10 10:01:46 +0100

    working create user form

--- src/components/TheWizkidCreate.vue
+++ src/components/TheWizkidCreate.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <button @click="createWizkid">Create Wizkid</button>
+  <div class="form-container">
+    <form @submit.prevent="createWizkid">
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" id="name" v-model="name" required>
+      </div>
+      <div class="form-group">
+        <label for="role">Role:</label>
+        <input type="number" id="role" v-model="role" required>
+      </div>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="email" required>
+      </div>
+      <button type="submit">Create Wizkid</button>
+    </form>
   </div>
 </template>
 
@@ -8,21 +22,77 @@
 import axios from 'axios';
 
 export default {
+  data() {
+    return {
+      name: '',
+      role: null,
+      email: '',
+    };
+  },
   methods: {
     async createWizkid() {
+      console.log(this.name, this.role, this.email);
       try {
         const response = await axios.post('http://localhost:8000/wizkids', {
-          name: 'derp',
-          role: 2,
-          email: 'derp@gmail.com',
+          name: this.name,
+          role: parseInt(this.role),
+          email: this.email,
         });
         console.log(response.data);
-        // Do something with the response, such as redirect to the new wizkid's page
+        alert('Wizkid created successfully!');
       } catch (error) {
         console.error(error);
-        // Handle error
+        alert('Oops! Something went wrong.');
       }
     },
   },
 };
 </script>
+
+<style>
+.form-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 2rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.form-group {
+  display: flex;
+  flex-direction: column;
+}
+
+label {
+  font-weight: bold;
+}
+
+input {
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 1rem;
+}
+
+button[type="submit"] {
+  background-color: #008CBA;
+  color: #fff;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 5px;
+  font-size: 1rem;
+  cursor: pointer;
+}
+
+button[type="submit"]:hover {
+  background-color: #005F6B;
+}
+</style>
