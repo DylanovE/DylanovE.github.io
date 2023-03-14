796b8ba979255782ecd2196891a8ccce622b1909
DylanovE <90382484+DylanovE@users.noreply.github.com>
2023-03-14 11:38:13 +0100

    adding buttons to edit and delete, also a message if nothing is found with searching

--- src/components/CRUD/TheWizkidRead.vue
+++ src/components/CRUD/TheWizkidRead.vue
@@ -18,6 +18,7 @@
               <span v-if="sortColumn === 'role'">{{ sortDirection === 1 ? 'Role&#x25b4;' : 'Role&#x25be;' }}</span>
               <span v-else>Role&#x25b4;&#x25be;</span>
             </th>
+            <th/>
           </tr>
         </thead>
         <tbody>
@@ -26,11 +27,23 @@
               <td>{{ wizkid.id }}</td>
               <td>{{ wizkid.name }}</td>
               <td>{{ wizkidRoleMap[wizkid.role] }}</td>
+              <td>
+                <svg @click="deleteWizkid(wizkid.id)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24" class="inline-block" role="presentation" >
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
+                  </path>
+                </svg>
+                <RouterLink :to="{ name: 'wizkids-update', params: {wizkidId: wizkid.id }, query: { name: wizkid.name, role: wizkid.role }}" style="color: var(--color-text);" >
+                  <svg   xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24" class="inline-block" role="presentation">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
+                    </path>
+                  </svg>
+                </RouterLink>
+              </td>
             </tr>
           </template>
           <template v-else>
             <tr>
-              <td colspan="3" class="text-center">No WizKids Found</td>
+              <td colspan="4" class="text-center">No WizKids Found</td>
             </tr>
           </template>
         </tbody>
