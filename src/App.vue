35a511f87a5ea5468cfc7e2e2a04c8584fdcf1be
DylanovE <90382484+DylanovE@users.noreply.github.com>
2023-03-28 14:17:35 +0200

    logout function improvements

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@
             <nav class="align-items-center">
                 <RouterLink to="/">Home</RouterLink>
                 <RouterLink to="/wizkid-manager">Wizkid manager</RouterLink>
-                <RouterLink v-if="isLoggedIn" to="" @click="logout()">logout</RouterLink>
-                <RouterLink v-if="!isLoggedIn" to="/login">login</RouterLink>
+                <RouterLink v-if="apiToken" to="" @click="logout()">logout</RouterLink>
+                <RouterLink v-if="!apiToken" to="/login">login</RouterLink>
             </nav>
         </div>
     </header>
@@ -23,10 +23,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {RouterLink, RouterView} from 'vue-router';
 import HelloWorld from '@/components/ui/IntroductionText.vue';
 import WizardTuxLogo from '@/components/icons/IconWizardTux.vue';
-const isLoggedIn = !!sessionStorage.apiToken;
+import {ref} from 'vue';
+const apiToken = ref(sessionStorage.apiToken);
+
 function logout() {
     sessionStorage.clear();
-    location.reload();
+    apiToken.value = sessionStorage.apiToken;
 }
 </script>
 
@@ -50,7 +52,6 @@ nav {
   display: flex;
   align-items: center;
   justify-content: center;
-
 }
 
 nav a.router-link-exact-active {
