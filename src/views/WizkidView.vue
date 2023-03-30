4660de5b5c43f203a19953711f4f5ee93c53ed86
DylanovE <90382484+DylanovE@users.noreply.github.com>
2023-03-30 10:19:40 +0200

    order fix

--- src/views/WizkidView.vue
+++ src/views/WizkidView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import WizkidManager from '@/components/ui/WizkidManager.vue';
-</script>
-
 <template>
     <main class="container wizkids mt-4">
         <WizkidManager />
     </main>
 </template>
+
+<script setup>
+import WizkidManager from '@/components/ui/WizkidManager.vue';
+</script>
